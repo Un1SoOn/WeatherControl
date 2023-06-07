0a9d1c22535be249b590b210a6060e6f90a43713
Mikhalev <pinmii@yandex.ru>
2023-06-07 19:25:29 +0400

    first commit

---
+++ src/test/java/ru/mikhalev/springprojects/WeatherControl/WeatherControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikhalev.springprojects.WeatherControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
